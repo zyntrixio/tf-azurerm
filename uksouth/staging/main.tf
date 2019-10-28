72b73262409cb2ce68c7bf2baa29df0c8eca2b04
Chris Pressland <mail@cpressland.io>
2019-10-28 21:40:39 +0000

    Increase Staging to 5 nodes

--- uksouth/staging/main.tf
+++ uksouth/staging/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~> 1.30.1"
+  version = "~> 1.35.0"
   subscription_id = "0add5c8e-50a6-4821-be0f-7a47c879b009"
   client_id = "98e2ee67-a52d-40fc-9b39-155887530a7b"
   tenant_id = "a6e2367a-92ea-4e5a-b565-723830bcc095"
