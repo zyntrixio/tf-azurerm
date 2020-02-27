4469db42751fad8c5dc858429fbbcfa4a9e2e890
Chris Pressland <mail@cpressland.io>
2020-02-27 22:31:44 +0000

    Increased worker count to 4 for Sandbox

--- uksouth/sandbox/vars.tf
+++ uksouth/sandbox/vars.tf
@@ -11,6 +11,6 @@ variable subnet_address_prefixes {
 }
 
 variable worker_vm_size { default = "Standard_D4s_v3" }
-variable worker_count { default = 3 }
+variable worker_count { default = 4 }
 variable controller_vm_size { default = "Standard_D2s_v3" }
 variable controller_count { default = 1 }
