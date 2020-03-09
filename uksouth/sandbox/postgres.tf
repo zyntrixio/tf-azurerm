46cf527ce59200b8af3ef5013c66928a7d9f86e4
Chris Pressland <mail@cpressland.io>
2020-03-09 17:48:30 +0000

    Updated Postgres for Sandbox

--- uksouth/sandbox/postgres.tf
+++ uksouth/sandbox/postgres.tf
@@ -1,30 +1,31 @@
-resource "random_password" "pg_pass" {
+resource "random_password" "performance" {
   length = 24
   special = false
 }
 
-resource "azurerm_postgresql_server" "postgres" {
-  name = "bink-sandbox-uksouth"
+resource "azurerm_postgresql_server" "performance" {
+  name = "bink-performance-sandbox-uksouth"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name = "GP_Gen5_2"
+  sku_name = "GP_Gen5_4"
 
   storage_profile {
-    storage_mb = 102400
+    storage_mb = 1024000
     backup_retention_days = 7
     geo_redundant_backup = "Disabled"
     auto_grow = "Disabled"
   }
 
   administrator_login = "laadmin"
-  administrator_login_password = random_password.pg_pass.result
+  administrator_login_password = random_password.performance.result
   version = 11
   ssl_enforcement = "Enabled"
 }
 
-resource "azurerm_postgresql_virtual_network_rule" "workers" {
+resource "azurerm_postgresql_virtual_network_rule" "performance" {
   name = "workers"
   resource_group_name = azurerm_resource_group.rg.name
-  server_name = azurerm_postgresql_server.postgres.name
+  server_name = azurerm_postgresql_server.performance.name
   subnet_id = azurerm_subnet.subnet.0.id
 }
+
