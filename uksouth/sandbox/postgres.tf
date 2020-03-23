ccf7d634e5fee5002b8e19431f7fb5a954eabf6b
Chris Pressland <mail@cpressland.io>
2020-03-23 14:25:32 +0000

    Added additional postgres and redis to sandbox

--- uksouth/sandbox/postgres.tf
+++ uksouth/sandbox/postgres.tf
@@ -29,3 +29,34 @@ resource "azurerm_postgresql_virtual_network_rule" "performance" {
   subnet_id = azurerm_subnet.subnet.0.id
 }
 
+resource "random_password" "oat" {
+  length = 24
+  special = false
+}
+
+resource "azurerm_postgresql_server" "oat" {
+  name = "bink-oat-sandbox-uksouth"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name = "GP_Gen5_4"
+
+  storage_profile {
+    storage_mb = 1024000
+    backup_retention_days = 7
+    geo_redundant_backup = "Disabled"
+    auto_grow = "Disabled"
+  }
+
+  administrator_login = "laadmin"
+  administrator_login_password = random_password.oat.result
+  version = 11
+  ssl_enforcement = "Enabled"
+}
+
+resource "azurerm_postgresql_virtual_network_rule" "oat" {
+  name = "workers"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name = azurerm_postgresql_server.oat.name
+  subnet_id = azurerm_subnet.subnet.0.id
+}
+
--- uksouth/sandbox/redis.tf
+++ uksouth/sandbox/redis.tf
@@ -1,5 +1,18 @@
-resource "azurerm_redis_cache" "redis" {
+resource "azurerm_redis_cache" "performance" {
   name = "bink-performance-sandbox-uksouth"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  capacity = 0
+  family = "C"
+  sku_name = "Standard"
+  enable_non_ssl_port = true
+  minimum_tls_version = "1.2"
+
+  redis_configuration {}
+}
+
+resource "azurerm_redis_cache" "oat" {
+  name = "bink-oat-sandbox-uksouth"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   capacity = 0
