3b7299f1087f0055e3b235d4115bf7abe5b08588
Chris Pressland <mail@cpressland.io>
2020-03-28 16:50:56 +0000

    Added Sandbox Postgres and Redis

--- uksouth/sandbox/postgres.tf
+++ uksouth/sandbox/postgres.tf
@@ -60,3 +60,34 @@ resource "azurerm_postgresql_virtual_network_rule" "oat" {
   subnet_id = azurerm_subnet.subnet.0.id
 }
 
+resource "random_password" "sandbox" {
+  length = 24
+  special = false
+}
+
+resource "azurerm_postgresql_server" "sandbox" {
+  name = "bink-sandbox-uksouth"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name = "GP_Gen5_2"
+
+  storage_profile {
+    storage_mb = 102400
+    backup_retention_days = 7
+    geo_redundant_backup = "Disabled"
+    auto_grow = "Disabled"
+  }
+
+  administrator_login = "laadmin"
+  administrator_login_password = random_password.sandbox.result
+  version = 11
+  ssl_enforcement = "Enabled"
+}
+
+resource "azurerm_postgresql_virtual_network_rule" "sandbox" {
+  name = "workers"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name = azurerm_postgresql_server.sandbox.name
+  subnet_id = azurerm_subnet.subnet.0.id
+}
+
