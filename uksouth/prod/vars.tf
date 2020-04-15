e2b54487b6d2fa54217d965b4c6a21f0c2c17080
Chris Pressland <mail@cpressland.io>
2020-04-16 00:43:27 +0100

    Resized D8s_v3 to D4s_v3 for Production Workers

--- uksouth/prod/vars.tf
+++ uksouth/prod/vars.tf
@@ -17,8 +17,8 @@ variable "tags" {
   }
 }
 
-variable worker_vm_size { default = "Standard_D8s_v3" }
-variable worker_count { default = 6 }
+variable worker_vm_size { default = "Standard_D4s_v3" }
+variable worker_count { default = 12 }
 variable controller_vm_size { default = "Standard_D4s_v3" }
 variable controller_count { default = 3 }
 variable etcd_vm_size { default = "Standard_D2s_v3" }
