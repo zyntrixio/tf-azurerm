08fb7d022bcbb72f464aa1e366044378f2399fd9
Chris Pressland <mail@cpressland.io>
2020-05-19 21:41:05 +0100

    Modify Prod to use new VM format

--- uksouth/prod/vms-worker.tf
+++ uksouth/prod/vms-worker.tf
@@ -44,100 +44,63 @@ resource "azurerm_network_interface" "worker" {
     }
 }
 
-resource "azurerm_virtual_machine" "worker" {
+resource "azurerm_linux_virtual_machine" "worker" {
     count = var.worker_count
     name = format("${var.environment}-worker-%02d", count.index + 1)
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
     availability_set_id = azurerm_availability_set.worker.id
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    depends_on = [azurerm_network_interface_backend_address_pool_association.worker-bap-assoc]
+    size = var.worker_vm_size
+    admin_username = "terraform"
+    tags = var.tags
+
     network_interface_ids = [
         element(azurerm_network_interface.worker.*.id, count.index),
     ]
-    vm_size = var.worker_vm_size
-    delete_os_disk_on_termination = true
-    delete_data_disks_on_termination = false
 
-    storage_image_reference {
+    admin_ssh_key {
+        username = "terraform"
+        public_key = file("~/.ssh/id_bink_azure_terraform.pub")
+    }
+
+    os_disk {
+        caching = "ReadOnly"
+        storage_account_type = "StandardSSD_LRS"
+        disk_size_gb = 128
+    }
+
+    source_image_reference {
         publisher = "Canonical"
         offer = "UbuntuServer"
         sku = "16.04-LTS"
         version = "latest"
     }
 
-    storage_os_disk {
-        name = format("${var.environment}-worker-%02d-disk", count.index + 1)
-        disk_size_gb = "128"
-        caching = "ReadOnly"
-        create_option = "FromImage"
-        managed_disk_type = "StandardSSD_LRS"
-    }
+    provisioner "chef" {
+        environment = "uksouth-prod"
+        client_options = ["chef_license 'accept'"]
+        run_list = ["role[worker]"]
+        node_name = self.name
+        server_url = "https://chef.uksouth.bink.sh:4444/organizations/bink"
+        recreate_client = true
+        user_name = "terraform"
+        user_key = file("./chef.pem")
+        version = "15.9.17"
+        ssl_verify_mode = ":verify_peer"
+        secret_key = "E9PXP4mDCH16RZIhNAD3CbKnCo/d/dPEROXV/Ui3oL94TqTSiZoBXCpBfUQGA+WzuucfLix8GL/AWlx7hkZMSgzqkielfc/PQ8hH8KGMT8NAiqKRGzxMDgxzCkVrrxqyO1CrH1NNUqbYAWaGNpUghLf62oHs+3ury2+mGuFz7I/F0HsI038uxOQHXhc0GbCWFEYqfm5EW/jPMuHG+3BkUI77c/JFBiEnxF/HtQmpUOtaJprX9B6r6KbGgmiZzn8BCMclWA7FpPME3AhZCyG51hhtzKzP8gyvO6UMrl502KjLaKI0bg9PhTaNcqQeWEVyUNLZLAn4A4bf7aERq5oGiJT7fCoTKVD5LJuko+MpN5P3DQTJMyu6phoPz8Y6zr+EA7Nd3wxbqnjvAJJQxBqKwPimZ8wAv3U03OG/OtnhYgu7Df461HDvABizTTrRJchkwGNMxW0L4KJZt04T313yeJV3FWBRTv/DoWOkxk5jxHYUdHppH5wuyeBJe18GhrfCeR/3gp3kndA7UQypw38ta5McmzFbC6OQym6ovVrRDA3qEiB61QlbhhRsMu/Drgx51lvL5ASBtKjsen+vDmmFHXYFK2aXlEUQFO3aDyzWUDhJyNh0bRi3L8E+FNZAzuhmlihlgLJSVtud9yO55SwSzubkFaNjvaYkkHx9oVXbA40="
 
-    os_profile {
-        computer_name = format("${var.environment}-worker-%02d", count.index + 1)
-        admin_username = "terraform"
-        custom_data = <<-EOF
-      #cloud-config
-      write_files:
-      - encoding: b64
-        content: RTlQWFA0bURDSDE2UlpJaE5BRDNDYktuQ28vZC9kUEVST1hWL1VpM29MOTRUcVRTaVpvQlhDcEJmVVFHQStXenV1Y2ZMaXg4R0wvQVdseDdoa1pNU2d6cWtpZWxmYy9QUThoSDhLR01UOE5BaXFLUkd6eE1EZ3h6Q2tWcnJ4cXlPMUNySDFOTlVxYllBV2FHTnBVZ2hMZjYyb0hzKzN1cnkyK21HdUZ6N0kvRjBIc0kwMzh1eE9RSFhoYzBHYkNXRkVZcWZtNUVXL2pQTXVIRyszQmtVSTc3Yy9KRkJpRW54Ri9IdFFtcFVPdGFKcHJYOUI2cjZLYkdnbWlaem44QkNNY2xXQTdGcFBNRTNBaFpDeUc1MWhodHpLelA4Z3l2TzZVTXJsNTAyS2pMYUtJMGJnOVBoVGFOY3FRZVdFVnlVTkxaTEFuNEE0YmY3YUVScTVvR2lKVDdmQ29US1ZENUxKdWtvK01wTjVQM0RRVEpNeXU2cGhvUHo4WTZ6citFQTdOZDN3eGJxbmp2QUpKUXhCcUt3UGltWjh3QXYzVTAzT0cvT3RuaFlndTdEZjQ2MUhEdkFCaXpUVHJSSmNoa3dHTk14VzBMNEtKWnQwNFQzMTN5ZUpWM0ZXQlJUdi9Eb1dPa3hrNWp4SFlVZEhwcEg1d3V5ZUJKZTE4R2hyZkNlUi8zZ3Aza25kQTdVUXlwdzM4dGE1TWNtekZiQzZPUXltNm92VnJSREEzcUVpQjYxUWxiaGhSc011L0RyZ3g1MWx2TDVBU0J0S2pzZW4rdkRtbUZIWFlGSzJhWGxFVVFGTzNhRHl6V1VEaEp5TmgwYlJpM0w4RStGTlpBenVobWxpaGxnTEpTVnR1ZDl5TzU1U3dTenVia0ZhTmp2YVlra0h4OW9WWGJBNDA9
-        owner: root:root
-        path: /etc/chef/encrypted_data_bag_secret
-        permissions: '0600'
-      chef:
-        install_type: "omnibus"
-        force_install: true
-        server_url: "https://chef.uksouth.bink.sh:4444/organizations/bink"
-        node_name: "${format("${var.environment}-worker-%02d", count.index + 1)}"
-        environment: "${var.resource_group_name}"
-        validation_name: "bink-validator"
-        validation_cert: |
-          -----BEGIN RSA PRIVATE KEY-----
-          MIIEpAIBAAKCAQEA35brXbKNGcY4owTgT0O1XmF0amkdCMAE7D+5BscnmgTAazmq
-          pkn1jf8sJftv6EIVrvXlUHBnjttu+wOqRPmXJkZHzBRsxi/oWvSzQsMpLoYc7s3D
-          ekX+A0LiRwTzlMcTCcEreUOXi3g11AwnXFYzd4jP5DmFRizR4ks1ajnHWIx7SCni
-          EQob9e9us/OFy8deiWgZi367pVWonZeISRfHFeyYIPVFtVThyb16fqrtXHkyOSLI
-          LAbZoAjP80wTgz5BIP9gcwE7w/HYfo5Bl8bSeHnPk2l9zXJiNYz2ULmDqIrBQl+y
-          2sFL7FUUPn/LiME8Hd0crQH8MxeSb5wuE2NYXwIDAQABAoIBAQDClWoyefB4TNz/
-          an/4G5ndEH0rGl1tPwdJv088Sdf6H0aCSoZr/5OXR0pZp8/FVrXeNRujfJ9zYR7d
-          j1wAeSKE6ccUIXZkqE7T0X+si4HsfkTxwtrrL7yXg/6/Bd0iTnoQFC/McfmSJETc
-          TNN4dYCG9+bM3Q5SezEReph64NvO/5rZmPZfGOSpb+uaHkY+SbWn3Q07iW0qqLm2
-          iC+AdPecOb7VTkO98APdN5C7TrixsVDLdXuA6x97T+wL87X0gaD8Dyf3Mlaoa052
-          Ek+pe+MgFPdZL25Z+RAzG4SBnOCGwsSRFpi0UQJMNjclkryrPxYzFw7Q8zuDEtEn
-          GJubLhW5AoGBAPI86/9EIGM9UO8tNly4dEA2Cm1uUKxCCPOfwafTpE9t3stPCDeO
-          C7FIpDZ8eSzuuJrQf4Ki4nY0KgrN+ZCoiGtkpDiuophWPFWZCTgRjsl0CQH93fzZ
-          VVDWn5IVIXVN+7fcjPIkJc2HlWKNeTBlf6QMfZWbBCt97RjnV0WZj2fjAoGBAOxK
-          xuNA5fz9Ra4aX9EKYkRuIclk5fRCWaneMlkfvddRgpS0dW/yh4x3sYPNxX1efKpK
-          8f6/CEw2yaebk6Fci7MM3XuFNywCZPGbhhRo5dEwm1agITbcTOMjhzRIJ82hsNMP
-          mnx5t9OiFNtU/qRyZw2EVKug/Cgtelm+ziBjxd5VAoGADVyc6+y3GKJGN5s31295
-          Qh14/8ZI/ud5lO6oAPjkpFj8JBzM6DuWm4XVQQgmqvrUBf4gOnV/pmOEOipYbMlP
-          FRVtFY9UerCvDU2uu4AEb0pOQOTe/NaEJTxheu5ddRoDG4Y35BdoWmjzLYd+OtQu
-          cT8bIkh1t2xvyqLgJn+s8F8CgYEAtfuZVejjuIavpbk2Vl7y89UGPH9zAz4epE82
-          46Eoqq5iLXkWgVN+xdZhZyuRkE63IMh7vEEQePIxON7/QmVuSkX8RmeA6GonqFSp
-          XQq1BPm0iXDmY8Qji0QPm1p/HUYMU2FPD9MGmv3Xply9iZV6fNSQCWcBDUiJVJk5
-          U4TEHckCgYBjZNvspUT7BjhMRTj+55Qu/vxe+W49XTRlTY4TzVcQL6sJ0X28qrnk
-          2Y4lblI7OXmwoXNsms4S3GvcszDZbsu16pm2MeUibP5MXRp1V9GRf9Y4Y0yrDvon
-          kGgAf033beTHYzJa3XIp0XhF7+7SLwaF4Imje4Bp8FKJmVirQT2OYw==
-          -----END RSA PRIVATE KEY-----
-        run_list:
-         - "role[worker]"
-        omnibus_url: "https://www.chef.io/chef/install.sh"
-        omnibus_version: "15.1.36"
-      runcmd:
-       - [ chef-client, --chef-license, accept ]
-      output: {all: '| tee -a /var/log/cloud-init-output.log'}
-
-    EOF
-    }
-
-    os_profile_linux_config {
-        disable_password_authentication = true
-        ssh_keys {
-            path = "/home/terraform/.ssh/authorized_keys"
-            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrdSta+Sv3YWupzHk4U1VS7jvUvkQgmWexanDnGHLx7YjBKxi1tuhE0WgzgkbB3WqDNLrj5dXdv9la8S9VvrL1L1r4YG+5N0f6Ri1xE+cGei6aFAm57eLPnGhAY6lxiPSx79x+cfmW0YdZHI/6rb4Gix+KoH4BOPZnshxjoyL5MJpel2/5LZHWuazT3ihzWXemhMQ11mXJGot+tuVRB3tkVg+vi//YyRo5vKQSjpvirrP8MgQY76jk0RzxhwsP1d+7lkeAcedPilNpmhP72rfWMTxkrbO7XQrZMpIeL7qywdaOb0tPEB0n9KscUwiMvM4oOLVizsgzKoUOZ91rkxhb id_bink_azure_terraform"
+        connection {
+            type = "ssh"
+            user = "terraform"
+            host = self.private_ip_address
+            private_key = file("~/.ssh/id_bink_azure_terraform")
+            bastion_host = "ssh.uksouth.bink.sh"
+            bastion_user = "terraform"
+            bastion_private_key = file("~/.ssh/id_bink_azure_terraform")
         }
     }
 
-    tags = var.tags
 }
 
 module "worker_nsg_rules" {
