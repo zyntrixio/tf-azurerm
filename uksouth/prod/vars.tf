2dded41ec094aa86cb81a04162eb2c60ae80bf79
Chris Pressland <mail@cpressland.io>
2020-05-19 21:58:08 +0100

    Scaled to 16 nodes

--- uksouth/prod/vars.tf
+++ uksouth/prod/vars.tf
@@ -25,7 +25,7 @@ variable "tags" {
 }
 
 variable worker_vm_size { default = "Standard_D4s_v3" }
-variable worker_count { default = 15 }
+variable worker_count { default = 16 }
 variable controller_vm_size { default = "Standard_D4s_v3" }
 variable controller_count { default = 3 }
 variable etcd_vm_size { default = "Standard_D2s_v3" }
