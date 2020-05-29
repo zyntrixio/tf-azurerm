dce2a0087f1397587fdddce0a4a13e34e0b99dbb
Terry Cain <tcain@bink.com>
2020-05-29 13:26:07 +0100

    Updated chef

--- uksouth/tableau/vm.tf
+++ uksouth/tableau/vm.tf
@@ -157,7 +157,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     source_image_reference {
         publisher = "Canonical"
         offer = "UbuntuServer"
-        sku = "18.04-LTS"
+        sku = "16.04-LTS"
         version = "latest"
     }
 
