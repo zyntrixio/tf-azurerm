f77f776288ea971d08c0d66ce6bb3ca7c72e7a40
Terry Cain <tcain@bink.com>
2020-06-09 17:24:12 +0100

    Fixed formatting

--- uksouth/tools/pypi.tf
+++ uksouth/tools/pypi.tf
@@ -1,17 +1,17 @@
 resource "azurerm_storage_account" "tools" {
-  name                     = "binktools"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  
-  account_kind = "BlobStorage"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+    name = "binktools"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
 
-  tags = var.tags
+    account_kind = "BlobStorage"
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+
+    tags = var.tags
 }
 
 resource "azurerm_storage_container" "pypi" {
-  name                  = "pypi"
-  storage_account_name  = azurerm_storage_account.tools.name
-  container_access_type = "private"
+    name = "pypi"
+    storage_account_name = azurerm_storage_account.tools.name
+    container_access_type = "private"
 }
