a7dbb181902d527b12dce6041ec97ee7fb8fd4d8
Terry Cain <tcain@bink.com>
2020-06-10 10:12:24 +0100

    Updated bifrost version

--- uksouth/staging/chef.tf
+++ uksouth/staging/chef.tf
@@ -1,7 +1,7 @@
 resource "chef_environment" "env" {
     name = var.resource_group_name
     cookbook_constraints = {
-        bifrost = "= 2.6.1"
+        bifrost = "= 2.6.3"
         romanoff = "= 2.1"
         fury = "= 1.5.0"
         jarvis = "= 2.0.0"
