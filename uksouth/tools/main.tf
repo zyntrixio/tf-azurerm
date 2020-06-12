317db5e039f3eea12791b465dba970518afff241
Terry Cain <tcain@bink.com>
2020-06-12 11:47:29 +0100

    Added identity

--- uksouth/tools/main.tf
+++ uksouth/tools/main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 data "azurerm_client_config" "current" {}
+
+resource "azurerm_user_assigned_identity" "prometheus" {
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+
+    name = "prometheus"
+}
