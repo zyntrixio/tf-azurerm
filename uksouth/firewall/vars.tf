1466e1366c07e772d4eba27d11605aa1d1117288
Terry Cain <tcain@bink.com>
2020-06-15 10:51:02 +0100

    Updated IP

--- uksouth/firewall/vars.tf
+++ uksouth/firewall/vars.tf
@@ -21,6 +21,6 @@ variable "secure_origins" {
         "217.169.3.233/32", # cpressland@bink.com
         "81.2.99.144/29", # cpressland@bink.com
         "82.13.29.15/32", # twinchester@bink.com
-        "86.28.118.165/32" # tcain@bink.com
+        "82.24.92.107/32" # tcain@bink.com
     ]
 }
