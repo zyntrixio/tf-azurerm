58ef5bcb5f72bc700d14a689a5f0d40892188b57
Chris Pressland <mail@cpressland.io>
2020-07-07 20:41:36 +0100

    linting..

--- uksouth/storage/main.tf
+++ uksouth/storage/main.tf
@@ -114,9 +114,9 @@ resource "azurerm_storage_account" "bink" {
 }
 
 resource "azurerm_role_assignment" "bink_iam_sperrygrove" {
-  scope = azurerm_storage_account.bink.id
-  role_definition_name = "Contributor"
-  principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
+    scope = azurerm_storage_account.bink.id
+    role_definition_name = "Contributor"
+    principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
 }
 
 resource "azurerm_storage_account" "harmonia" {
@@ -140,9 +140,9 @@ resource "azurerm_storage_account" "harmoniastaging" {
 }
 
 resource "azurerm_role_assignment" "harmoniastaging_iam_sperrygrove" {
-  scope = azurerm_storage_account.harmoniastaging.id
-  role_definition_name = "Contributor"
-  principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
+    scope = azurerm_storage_account.harmoniastaging.id
+    role_definition_name = "Contributor"
+    principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
 }
 
 resource "azurerm_storage_account" "harmoniadev" {
@@ -156,9 +156,9 @@ resource "azurerm_storage_account" "harmoniadev" {
 }
 
 resource "azurerm_role_assignment" "harmoniadev_iam_sperrygrove" {
-  scope = azurerm_storage_account.harmoniadev.id
-  role_definition_name = "Contributor"
-  principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
+    scope = azurerm_storage_account.harmoniadev.id
+    role_definition_name = "Contributor"
+    principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
 }
 
 resource "azurerm_storage_account" "thoughtspot" {
