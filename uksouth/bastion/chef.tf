be1f82a9e3554df0c3bfb4b92b8c23e82de0091f
Terry Cain <tcain@bink.com>
2020-07-09 14:16:29 +0100

    Updated bastion cookbook

--- uksouth/bastion/chef.tf
+++ uksouth/bastion/chef.tf
@@ -1,11 +1,15 @@
 resource "chef_environment" "env" {
     name = azurerm_resource_group.rg.name
+    cookbook_constraints = {
+        nebula = "= 2.0.0"
+    }
 }
 
 resource "chef_role" "role" {
     name = "bastion"
     run_list = [
         "recipe[fury]",
-        "recipe[jarvis]"
+        "recipe[jarvis]",
+        "recipe[nebula]"
     ]
 }
