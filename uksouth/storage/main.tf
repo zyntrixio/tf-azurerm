e932b067244c7c9bb712bbd39649ff3d973dcfda
Chris Pressland <mail@cpressland.io>
2020-07-14 23:57:36 +0100

    added mids storage account

--- uksouth/storage/main.tf
+++ uksouth/storage/main.tf
@@ -119,6 +119,16 @@ resource "azurerm_role_assignment" "bink_iam_sperrygrove" {
     principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
 }
 
+resource "azurerm_storage_account" "mids" {
+    name = "mids"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    account_kind = "BlobStorage"
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    enable_https_traffic_only = true
+}
+
 resource "azurerm_storage_account" "harmonia" {
     name = "harmonia"
     resource_group_name = azurerm_resource_group.rg.name
