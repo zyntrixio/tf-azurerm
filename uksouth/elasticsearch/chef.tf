66c8d03b274a87fd1498087a9dc950457f068d65
Terry Cain <tcain@bink.com>
2020-07-27 13:54:12 +0100

    Bumped manhattan version

--- uksouth/elasticsearch/chef.tf
+++ uksouth/elasticsearch/chef.tf
@@ -1,7 +1,7 @@
 resource "chef_environment" "env" {
     name = var.resource_group_name
     cookbook_constraints = {
-        manhattan = "= 2.0.1"
+        manhattan = "= 2.0.2"
         romanoff = ">= 2.0.2"
         fury = ">= 1.5.1"
         jarvis = ">= 2.1.0"
