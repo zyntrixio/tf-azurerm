ddb4ee0ccadc7684de329522e1e483887fd30306
Terry Cain <tcain@bink.com>
2020-08-18 12:03:24 +0100

    Updated tableau chef attributes

--- uksouth/tableau/chef.tf
+++ uksouth/tableau/chef.tf
@@ -10,7 +10,12 @@ resource "chef_environment" "env" {
     default_attributes_json = jsonencode({
         "rorschach" : {
             "domain" : "tableau.uksouth.bink.sh",
-            "port" : 8080
+            "port" : 8080,
+            "nginx" : {
+                "proxy_read_timeout" : 600,
+                "proxy_send_timeout" : 600,
+                "client_max_body_size" : 500,
+            }
         }
     })
 }
