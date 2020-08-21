4051cd07fceb4a22be7c4a461bba6c7475889224
Chris Pressland <mail@cpressland.io>
2020-08-21 08:26:40 +0100

    Build out Dev & Staging

--- uksouth-staging.tf
+++ uksouth-staging.tf
@@ -46,3 +46,62 @@ module "uksouth_staging_environment" {
         },
     }
 }
+
+module "uksouth_staging_cluster_0" {
+    source = "./modules/cluster"
+    providers = {
+        azurerm = azurerm.uk_staging
+        azurerm.core = azurerm
+    }
+
+    resource_group_name = "uksouth-staging-k0"
+    cluster_name = "staging0"
+    location = "uksouth"
+    vnet_cidr = "10.128.0.0/16"
+    eventhub_authid = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
+
+    worker_count = 3
+
+    # Gitops repo, Managed identity for syncing common secrets
+    gitops_repo = "git@git.bink.com:GitOps/uksouth-staging.git"
+    common_keyvault = data.terraform_remote_state.uksouth-common.outputs.keyvault
+    common_keyvault_sync_identity = data.terraform_remote_state.uksouth-common.outputs.keyvault2kube_identity
+
+    # DNS zones
+    private_dns = module.uksouth-dns.private_dns
+    public_dns = module.uksouth-dns.public_dns
+
+    # Peers    
+    peers = {
+        firewall = {
+            vnet_id = module.uksouth-firewall.vnet_id
+            vnet_name = module.uksouth-firewall.vnet_name
+            resource_group_name = module.uksouth-firewall.resource_group_name
+        }
+        elasticsearch = {
+            vnet_id = module.uksouth-elasticsearch.vnet_id
+            vnet_name = module.uksouth-elasticsearch.vnet_name
+            resource_group_name = module.uksouth-elasticsearch.resource_group_name
+        }
+    }
+
+    firewall = {
+        firewall_name = module.uksouth-firewall.firewall_name
+        resource_group_name = module.uksouth-firewall.resource_group_name
+        ingress_priority = 600
+        rule_priority = 600
+        public_ip = module.uksouth-firewall.public_ips.1.ip_address
+        secure_origins = local.secure_origins
+        developer_ips = local.developer_ips
+        ingress_source = "*"
+        ingress_http = 8000
+        ingress_https = 4000
+        ingress_controller = 6000
+    }
+
+    postgres_servers = module.uksouth_staging_environment.postgres_servers
+
+    tags = {
+        "Environment" = "Staging",
+    }
+}
