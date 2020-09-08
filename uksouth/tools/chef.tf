fbcb0b99cb58f05317cebad02a11dc63a834ceca
Terry Cain <tcain@bink.com>
2020-09-08 15:52:13 +0100

    Converted tools to 4th gen

--- uksouth/tools/chef.tf
+++ uksouth/tools/chef.tf
@@ -1,20 +1,46 @@
 resource "chef_environment" "env" {
     name = var.resource_group_name
     cookbook_constraints = {
-        bifrost = "= 2.9.0"
+        bifrost = "= 4.1.0"
         romanoff = ">= 2.0.2"
         fury = ">= 1.6.1"
         nebula = "= 2.1.0"
     }
 
     default_attributes_json = jsonencode({
-        "flux" : {
-            "repo" : var.gitops_repo,
-        },
         "common_secrets" : {
             "keyvault_url" : var.common_keyvault.url,
             "keyvault2kube_resourceid" : var.common_keyvault_sync_identity.resource_id,
             "keyvault2kube_clientid" : var.common_keyvault_sync_identity.client_id
+        },
+
+        # New stuff
+        "kubernetes" : {
+            "api" : {
+                "host" : "tools.k8s.uksouth.bink.sh",
+                "ipaddress" : cidrhost(cidrsubnet(var.address_space, 8, 64), 4)  # Removes depenency on subnet
+            }
+        },
+        "flux" : {
+            "repo" : var.gitops_repo,
+        },
+        "azure" : {
+            "keyvault" : {
+                "url" : var.common_keyvault.url,
+                "resource_id" : var.common_keyvault_sync_identity.resource_id,
+                "client_id" : var.common_keyvault_sync_identity.client_id
+            },
+            "config" : {
+                "subscription_id" : data.azurerm_subscription.current.subscription_id,
+                "resource_group" : azurerm_resource_group.rg.name,
+                "route_table_name" : azurerm_route_table.rt.name,
+                "vnet_name" : azurerm_virtual_network.vnet.name,
+                "vnet_resource_group" : azurerm_resource_group.rg.name,
+                "subnet_name" : azurerm_subnet.subnet[0].name,
+                "security_group_name" : azurerm_network_security_group.nsg[0].name,
+                "primary_availability_set_name" : azurerm_availability_set.worker.name,
+                "worker_cidr" : cidrsubnet(var.address_space, 2, 0)
+            }
         }
     })
 }
