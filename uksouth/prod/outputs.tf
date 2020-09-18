833677d682b4c68933279da10fc9fe08dc36b4b8
Chris Pressland <mail@cpressland.io>
2020-09-18 16:07:52 +0100

    Git Cleanup

--- uksouth/prod/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnet_cidrs" {
-    value = zipmap(var.subnet_names, var.subnet_address_prefixes)
-}
-
-output "subnet_ids" {
-    value = zipmap(var.subnet_names, azurerm_subnet.subnet.*.id)
-}
--- uksouth/staging/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = "uksouth"
-
-    tags = var.tags
-}
-
-data "azurerm_client_config" "current" {}
