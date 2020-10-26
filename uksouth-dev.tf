5d2ebc8ee126b70848821edb6c379dc0fdd4cc31
Chris Pressland <mail@cpressland.io>
2020-10-26 15:08:13 +0000

    Release Bifrost 4.2.2

--- uksouth-dev.tf
+++ uksouth-dev.tf
@@ -63,7 +63,7 @@ module "uksouth_dev_cluster_0" {
     location = "uksouth"
     vnet_cidr = "10.99.0.0/16"
     eventhub_authid = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
-    bifrost_version = "4.2.1"
+    bifrost_version = "4.2.2"
     ubuntu_version = "20.04"
     controller_vm_size = "Standard_D2s_v4"
     worker_vm_size = "Standard_D4s_v4"
