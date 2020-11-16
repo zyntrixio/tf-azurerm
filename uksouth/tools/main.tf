028bb5bd1965a34b608a99a48bf440a653dee4a8
Terry Cain <tcain@bink.com>
2020-11-16 17:13:59 +0000

    Adjusted prometheus roles

--- uksouth/tools/main.tf
+++ uksouth/tools/main.tf
@@ -42,3 +42,21 @@ resource "azurerm_role_assignment" "prometheus_azure_vm_read" {
     role_definition_id = azurerm_role_definition.prometheus_azure_vm_read.role_definition_resource_id
     principal_id = azurerm_user_assigned_identity.prometheus.principal_id
 }
+
+locals {
+    subs = toset([
+        "/subscriptions/79560fde-5831-481d-8c3c-e812ef5046e5",
+        "/subscriptions/6e685cd8-73f6-4aa6-857c-04ed9b21d17d",
+        "/subscriptions/457b0db5-6680-480f-9e77-2dafb06bd9dc",
+        "/subscriptions/794aa787-ec6a-40dd-ba82-0ad64ed51639",
+        "/subscriptions/957523d8-bbe2-4f68-8fae-95975157e91c"
+    ])
+}
+
+resource "azurerm_role_assignment" "prometheus_azure_vm_read_subs" {
+    for_each = local.subs
+
+    scope = each.value
+    role_definition_id = azurerm_role_definition.prometheus_azure_vm_read.role_definition_resource_id
+    principal_id = azurerm_user_assigned_identity.prometheus.principal_id
+}
