84d8e7447db1155d3515ef6ceee8deff519ab672
Chris Pressland <mail@cpressland.io>
2020-11-19 14:28:38 +0000

    Removed old unused storage accounts

--- uksouth/storage/main.tf
+++ uksouth/storage/main.tf
@@ -124,28 +124,6 @@ resource "azurerm_storage_account" "binkgitlabbackups" {
     min_tls_version = "TLS1_2"
 }
 
-resource "azurerm_storage_account" "aphrodite" {
-    name = "aphrodite"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = "westeurope"
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "GRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
-resource "azurerm_storage_account" "aphroditestaging" {
-    name = "aphroditestaging"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = "westeurope"
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
 resource "azurerm_storage_account" "bink" {
     name = "bink"
     resource_group_name = azurerm_resource_group.rg.name
@@ -174,73 +152,6 @@ resource "azurerm_storage_account" "mids" {
     min_tls_version = "TLS1_2"
 }
 
-resource "azurerm_storage_account" "harmonia" {
-    name = "harmonia"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
-resource "azurerm_storage_account" "harmoniapreprod" {
-    name = "harmoniapreprod"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
-resource "azurerm_storage_account" "harmoniastaging" {
-    name = "harmoniastaging"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
-resource "azurerm_role_assignment" "harmoniastaging_iam_sperrygrove" {
-    scope = azurerm_storage_account.harmoniastaging.id
-    role_definition_name = "Contributor"
-    principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
-}
-
-resource "azurerm_storage_account" "harmoniadev" {
-    name = "harmoniadev"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
-resource "azurerm_role_assignment" "harmoniadev_iam_sperrygrove" {
-    scope = azurerm_storage_account.harmoniadev.id
-    role_definition_name = "Contributor"
-    principal_id = "c7c13573-de9a-443e-a1a7-cc272cb26e2e"
-}
-
-resource "azurerm_storage_account" "thoughtspot" {
-    name = "thoughtspot"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = "westeurope"
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
 resource "azurerm_container_registry" "binkops" {
     name = "binkops"
     resource_group_name = azurerm_resource_group.rg.name
@@ -257,15 +168,4 @@ resource "azurerm_container_registry" "olympus" {
     sku = "Premium"
     admin_enabled = true
     georeplication_locations = ["UK South", "UK West"]
-}
-
-resource "azurerm_storage_account" "stagingsftp" {
-    name = "stagingsftp"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = "uksouth"
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
 }
