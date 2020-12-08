7d43744744fae1d62ce9fd8a73ddda83a1404bad
Terry Cain <tcain@bink.com>
2020-12-08 14:25:28 +0000

    Removed unused files

--- uksouth/sandbox/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-    name = "uksouth-sandbox"
-    location = "uksouth"
-
-    tags = var.tags
-}
-
-data "azurerm_client_config" "current" {}
--- uksouth/tools/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = "uksouth"
-
-    tags = var.tags
-}
-
