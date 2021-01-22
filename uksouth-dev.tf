3dcf1e740442bf02cf61099d040cbf885d9325b0
Terry Cain <tcain@bink.com>
2021-01-22 10:22:01 +0000

    Deployed DWH

--- uksouth-dev.tf
+++ uksouth-dev.tf
@@ -1,5 +1,5 @@
 module "uksouth_dev_environment" {
-    source = "git::ssh://git@git.bink.com/Terraform/azurerm_environment.git?ref=1.4.3"
+    source = "git::ssh://git@git.bink.com/Terraform/azurerm_environment.git?ref=1.5.3"
     providers = {
         azurerm = azurerm.uk_dev
     }
@@ -23,6 +23,9 @@ module "uksouth_dev_environment" {
     keyvault_users = {
         Backend = { object_id = "219194f6-b186-4146-9be7-34b731e19001" },
         QA = { object_id = "2e3dc1d0-e6b8-4ceb-b1ae-d7ce15e2150d" },
+    }
+    infra_keyvault_users = {
+        AzureSynapse = { object_id = module.uksouth_dev_datawarehouse.synapse_identity.principal_id, permissions = ["get"]}
     }
 
     postgres_config = {
