0544c54af106c0f4aa7fd9452c425b9abd311528
Chris Pressland <mail@cpressland.io>
2021-02-11 13:02:22 +0000

    One more for good luck

--- uksouth-perf.tf
+++ uksouth-perf.tf
@@ -70,7 +70,7 @@ module "uksouth_performance_cluster_0" {
     bifrost_version = "4.6.2"
     ubuntu_version = "20.04"
     controller_vm_size = "Standard_D2s_v4"
-    worker_vm_size = "Standard_D32s_v4"
+    worker_vm_size = "Standard_D4s_v4"
     worker_scaleset_size = 20
     use_scaleset = true
 
