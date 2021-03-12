fad529a342fdc3dc03c108302e8ace10d3ef182e
Terry Cain <tcain@bink.com>
2021-03-12 18:49:33 +0000

    Removed redis enterprise from dev

--- uksouth-dev.tf
+++ uksouth-dev.tf
@@ -1,5 +1,5 @@
 module "uksouth_dev_environment" {
-    source = "git::ssh://git@git.bink.com/Terraform/azurerm_environment.git?ref=1.6.1"
+    source = "git::ssh://git@git.bink.com/Terraform/azurerm_environment.git?ref=remove_servicebus"
     providers = {
         azurerm = azurerm.uk_dev
     }
@@ -41,6 +41,7 @@ module "uksouth_dev_environment" {
             name = "bink-uksouth-dev-common",
         },
     }
+
     redis_patch_schedule = {
         day_of_week = "Monday"
         start_hour_utc = 1
@@ -53,28 +54,28 @@ module "uksouth_dev_environment" {
             account_tier = "Standard"
         },
     }
-    # storage_management_policy_config = {
-    #     common = [
-    #         {
-    #             name = "backupshourly",
-    #             enabled = true,
-    #             prefix_match = ["backups/hourly"],
-    #             delete_after_days = 30
-    #         },
-    #         {
-    #             name = "backupsweekly",
-    #             enabled = true,
-    #             prefix_match = ["backups/weekly"],
-    #             delete_after_days = 90
-    #         },
-    #         {
-    #             name = "backupsyearly",
-    #             enabled = true,
-    #             prefix_match = ["backups/yearly"],
-    #             delete_after_days = 1095
-    #         }
-    #     ]
-    # }
+    storage_management_policy_config = {
+        common = [
+            {
+                name = "backupshourly",
+                enabled = true,
+                prefix_match = ["backups/hourly"],
+                delete_after_days = 30
+            },
+            {
+                name = "backupsweekly",
+                enabled = true,
+                prefix_match = ["backups/weekly"],
+                delete_after_days = 90
+            },
+            {
+                name = "backupsyearly",
+                enabled = true,
+                prefix_match = ["backups/yearly"],
+                delete_after_days = 1095
+            }
+        ]
+    }
     cert_manager_zone_id = module.uksouth-dns.bink-sh[2]
 }
 
