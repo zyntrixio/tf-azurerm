a42fe898ef8363ac990a7e4c90ffb369d4590c47
Terry Cain <tcain@bink.com>
2021-03-17 16:13:11 +0000

    Added bink.host mx

--- uksouth/dns/bink.host.tf
+++ uksouth/dns/bink.host.tf
@@ -7,3 +7,75 @@ resource "azurerm_private_dns_zone" "uksouth-bink-host" {
     name = "uksouth.bink.host"
     resource_group_name = azurerm_resource_group.rg.name
 }
+
+locals {
+    bink_host = {
+        cname_records = {
+            "autodiscover" = "autodiscover.outlook.com",
+        }
+        mx_records = {
+            "@" = [
+                {
+                    preference = 0,
+                    exchange = "bink-host.mail.protection.outlook.com",
+                }
+            ]
+        }
+        txt_records = {
+            "@" = [
+                "v=spf1 include:spf.protection.outlook.com -all",
+                "MS=ms43009780",
+            ]
+        }
+    }
+}
+
+resource "azurerm_dns_cname_record" "bink_host_cname" {
+    for_each = local.bink_host.cname_records
+
+    name = each.key
+    zone_name = azurerm_dns_zone.bink-host.name
+    resource_group_name = azurerm_resource_group.rg.name
+    ttl = 300
+    record = each.value
+}
+
+resource "azurerm_dns_mx_record" "bink_host_mx" {
+    for_each = local.bink_host.mx_records
+
+    name = each.key
+    zone_name = azurerm_dns_zone.bink-host.name
+    resource_group_name = azurerm_resource_group.rg.name
+    ttl = 300
+
+    dynamic "record" {
+        for_each = [for i in each.value : {
+            preference = i["preference"]
+            exchange = i["exchange"]
+        }]
+
+        content {
+            preference = record.value.preference
+            exchange = record.value.exchange
+        }
+    }
+}
+
+resource "azurerm_dns_txt_record" "bink_host_txt" {
+    for_each = local.bink_host.txt_records
+
+    name = each.key
+    zone_name = azurerm_dns_zone.bink-host.name
+    resource_group_name = azurerm_resource_group.rg.name
+    ttl = 300
+
+    dynamic "record" {
+        for_each = [for i in each.value : {
+            value = i
+        }]
+
+        content {
+            value = record.value.value
+        }
+    }
+}
