fec7a4e92e66c402007492c1462b2ef69919ca99
Chris Pressland <mail@cpressland.io>
2021-04-13 18:04:53 +0100

    Update environment

--- uksouth-tools.tf
+++ uksouth-tools.tf
@@ -1,5 +1,5 @@
 module "uksouth_tools_environment" {
-    source = "git::ssh://git@git.bink.com/Terraform/azurerm_environment.git?ref=1.7.5"
+    source = "git::ssh://git@git.bink.com/Terraform/azurerm_environment.git?ref=1.7.6"
     providers = {
         azurerm = azurerm
     }
