8afe8de13c3a57f8e320ce24fc53f078242c5f8c
Terry Cain <tcain@bink.com>
2021-05-26 16:05:47 +0100

    Killed preprod

--- uksouth-preprod.tf
+++ uksouth-preprod.tf
@@ -32,52 +32,52 @@ module "uksouth_preprod_environment" {
         Backend = "219194f6-b186-4146-9be7-34b731e19001",
     }
 
-    postgres_config = {
-        common = {
-            name = "bink-uksouth-preprod-common",
-            sku_name = "GP_Gen5_2",
-            storage_gb = 500,
-            public_access = true,
-            databases = ["atlas", "europa", "pontus", "thanatos", "zagreus"]
-        },
-        hermes = {
-            name = "bink-uksouth-preprod-hermes",
-            sku_name = "GP_Gen5_2",
-            storage_gb = 500,
-            public_access = true,
-            databases = ["hermes"]
-        },
-        hades = {
-            name = "bink-uksouth-preprod-hades",
-            sku_name = "GP_Gen5_2",
-            storage_gb = 500,
-            public_access = true,
-            databases = ["hades"]
-        },
-        harmonia = {
-            name = "bink-uksouth-preprod-harmonia",
-            sku_name = "GP_Gen5_4",
-            storage_gb = 500,
-            public_access = true,
-            databases = ["harmonia"]
-        },
-        polaris = {
-            name = "bink-uksouth-preprod-polaris",
-            sku_name = "GP_Gen5_4",
-            storage_gb = 500,
-            public_access = true,
-            databases = ["polaris"]
-        },
-    }
-    redis_config = {
-        common = {
-            name = "bink-uksouth-preprod-common",
-        },
-    }
-    redis_patch_schedule = {
-        day_of_week = "Wednesday"
-        start_hour_utc = 1
-    }
+    # postgres_config = {
+    #     common = {
+    #         name = "bink-uksouth-preprod-common",
+    #         sku_name = "GP_Gen5_2",
+    #         storage_gb = 500,
+    #         public_access = true,
+    #         databases = ["atlas", "europa", "pontus", "thanatos", "zagreus"]
+    #     },
+    #     hermes = {
+    #         name = "bink-uksouth-preprod-hermes",
+    #         sku_name = "GP_Gen5_2",
+    #         storage_gb = 500,
+    #         public_access = true,
+    #         databases = ["hermes"]
+    #     },
+    #     hades = {
+    #         name = "bink-uksouth-preprod-hades",
+    #         sku_name = "GP_Gen5_2",
+    #         storage_gb = 500,
+    #         public_access = true,
+    #         databases = ["hades"]
+    #     },
+    #     harmonia = {
+    #         name = "bink-uksouth-preprod-harmonia",
+    #         sku_name = "GP_Gen5_4",
+    #         storage_gb = 500,
+    #         public_access = true,
+    #         databases = ["harmonia"]
+    #     },
+    #     polaris = {
+    #         name = "bink-uksouth-preprod-polaris",
+    #         sku_name = "GP_Gen5_4",
+    #         storage_gb = 500,
+    #         public_access = true,
+    #         databases = ["polaris"]
+    #     },
+    # }
+    # redis_config = {
+    #     common = {
+    #         name = "bink-uksouth-preprod-common",
+    #     },
+    # }
+    # redis_patch_schedule = {
+    #     day_of_week = "Wednesday"
+    #     start_hour_utc = 1
+    # }
     eventhub_authid = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
     storage_config = {
         common = {
@@ -112,67 +112,69 @@ module "uksouth_preprod_environment" {
     managed_identities = local.managed_identities
 }
 
-module "uksouth_preprod_cluster_1" {
-    source = "git::ssh://git@git.bink.com/Terraform/azurerm_cluster.git?ref=2.4.2"
-    providers = {
-        azurerm = azurerm.uk_preprod
-        azurerm.core = azurerm
-    }
+# module "uksouth_preprod_cluster_0" {
+#     source = "git::ssh://git@git.bink.com/Terraform/azurerm_cluster.git?ref=2.4.4"
+#     providers = {
+#         azurerm = azurerm.uk_preprod
+#         azurerm.core = azurerm
+#     }
 
-    resource_group_name = "uksouth-preprod-k1"
-    cluster_name = "preprod0"
-    location = "uksouth"
-    vnet_cidr = "10.69.0.0/16"
-    eventhub_authid = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
+#     resource_group_name = "uksouth-preprod-k0"
+#     cluster_name = "preprod0"
+#     location = "uksouth"
+#     vnet_cidr = "10.69.0.0/16"
+#     eventhub_authid = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
 
-    bifrost_version = "4.7.2"
-    ubuntu_version = "20.04"
-    controller_vm_size = "Standard_D2s_v4"
-    worker_vm_size = "Standard_D4s_v4"
-    worker_count = 5
+#     bifrost_version = "4.8.3"
+#     ubuntu_version = "20.04"
+#     controller_vm_size = "Standard_D2as_v4"
+#     worker_vm_size = "Standard_D4s_v4"
+#     worker_scaleset_size = 5
+#     use_scaleset = true
+#     max_pods_per_host = 100
 
-    prometheus_subnet = "10.33.0.0/18"
+#     prometheus_subnet = "10.33.0.0/18"
 
-    # Gitops repo, Managed identity for syncing common secrets
-    gitops_repo = "git@git.bink.com:GitOps/uksouth-preprod.git"
-    common_keyvault = data.terraform_remote_state.uksouth-common.outputs.keyvault
-    common_keyvault_sync_identity = data.terraform_remote_state.uksouth-common.outputs.keyvault2kube_identity
+#     flux_environment = "uksouth-preprod"
 
-    # DNS zones
-    private_dns = module.uksouth-dns.private_dns
-    public_dns = module.uksouth-dns.public_dns
+#     common_keyvault = data.terraform_remote_state.uksouth-common.outputs.keyvault
+#     common_keyvault_sync_identity = data.terraform_remote_state.uksouth-common.outputs.keyvault2kube_identity
 
-    # Peers    
-    peers = {
-        firewall = {
-            vnet_id = module.uksouth-firewall.vnet_id
-            vnet_name = module.uksouth-firewall.vnet_name
-            resource_group_name = module.uksouth-firewall.resource_group_name
-        }
-        elasticsearch = {
-            vnet_id = module.uksouth-elasticsearch.vnet_id
-            vnet_name = module.uksouth-elasticsearch.vnet_name
-            resource_group_name = module.uksouth-elasticsearch.resource_group_name
-        }
-    }
+#     # DNS zones
+#     private_dns = module.uksouth-dns.private_dns
+#     public_dns = module.uksouth-dns.public_dns
 
-    firewall = {
-        firewall_name = module.uksouth-firewall.firewall_name
-        resource_group_name = module.uksouth-firewall.resource_group_name
-        ingress_priority = 800
-        rule_priority = 800
-        public_ip = module.uksouth-firewall.public_ips.15.ip_address
-        secure_origins = local.secure_origins
-        developer_ips = local.developer_ips
-        ingress_source = "*"
-        ingress_http = 8000
-        ingress_https = 4000
-        ingress_controller = 6000
-    }
+#     # Peers    
+#     peers = {
+#         firewall = {
+#             vnet_id = module.uksouth-firewall.vnet_id
+#             vnet_name = module.uksouth-firewall.vnet_name
+#             resource_group_name = module.uksouth-firewall.resource_group_name
+#         }
+#         elasticsearch = {
+#             vnet_id = module.uksouth-elasticsearch.vnet_id
+#             vnet_name = module.uksouth-elasticsearch.vnet_name
+#             resource_group_name = module.uksouth-elasticsearch.resource_group_name
+#         }
+#     }
 
-    postgres_servers = module.uksouth_preprod_environment.postgres_servers
+#     firewall = {
+#         firewall_name = module.uksouth-firewall.firewall_name
+#         resource_group_name = module.uksouth-firewall.resource_group_name
+#         ingress_priority = 800
+#         rule_priority = 800
+#         public_ip = module.uksouth-firewall.public_ips.15.ip_address
+#         secure_origins = local.secure_origins
+#         developer_ips = local.developer_ips
+#         ingress_source = "*"
+#         ingress_http = 8000
+#         ingress_https = 4000
+#         ingress_controller = 6000
+#     }
 
-    tags = {
-        "Environment" = "Pre-Production",
-    }
-}
+#     postgres_servers = module.uksouth_preprod_environment.postgres_servers
+
+#     tags = {
+#         "Environment" = "Pre-Production",
+#     }
+# }
