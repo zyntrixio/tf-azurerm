ff728c0d1c7de8a3c75ac44d78b2c281328c71ed
Chris Pressland <mail@cpressland.io>
2021-07-01 21:49:33 +0100

    Upgraded Bifrost

--- uksouth-perf.tf
+++ uksouth-perf.tf
@@ -127,7 +127,7 @@ module "uksouth_performance_cluster_0" {
     location = "uksouth"
     vnet_cidr = "10.43.0.0/16"
     eventhub_authid = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
-    bifrost_version = "4.8.3"
+    bifrost_version = "4.8.4"
     ubuntu_version = "20.04"
     controller_vm_size = "Standard_D2as_v4"
     worker_vm_size = "Standard_D4s_v4"
