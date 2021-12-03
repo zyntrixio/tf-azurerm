8e7f6525dea85a926d2aeb4ec96e74c8a0918c0e
Chris Pressland <mail@cpressland.io>
2021-12-03 14:27:29 +0000

    Fix diagnostic settings complaints

--- uksouth/frontdoor/main.tf
+++ uksouth/frontdoor/main.tf
@@ -1010,7 +1010,6 @@ resource "azurerm_monitor_diagnostic_setting" "diags" {
     eventhub_name = "azurefrontdoorpre" # go to a "pre" eventhub for post processing.
     eventhub_authorization_rule_id = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
     log_analytics_workspace_id = var.loganalytics_id
-    log_analytics_destination_type = "AzureDiagnostics"
 
     log {
         category = "FrontdoorAccessLog"
