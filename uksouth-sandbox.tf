bb809dbaa5b2d0463cce268bd4eaa4c6bb1cb066
Chris Pressland <mail@cpressland.io>
2022-02-18 17:49:31 +0000

    Removed the last single server!!!!

--- uksouth-sandbox.tf
+++ uksouth-sandbox.tf
@@ -104,16 +104,6 @@ module "uksouth_sandbox_environment" {
     }
   }
 
-  postgres_config = {
-    common = {
-      name          = "bink-uksouth-sandbox-common",
-      sku_name      = "GP_Gen5_4",
-      storage_gb    = 1000,
-      public_access = true,
-      databases     = ["*"]
-    },
-  }
-
   secret_namespaces = "default,oat,sit-barclays,sit-lbg,monitoring"
   eventhub_authid   = "/subscriptions/0add5c8e-50a6-4821-be0f-7a47c879b009/resourceGroups/uksouth-eventhubs/providers/Microsoft.EventHub/namespaces/binkuksouthlogs/authorizationRules/RootManageSharedAccessKey"
   storage_config = {
