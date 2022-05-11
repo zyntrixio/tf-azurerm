9b5ffc414aecf9eae6d9be047d3f2a9f7fd151d7
Nathan Read <nathancread@outlook.com>
2022-05-11 16:17:09 +0100

    adding mimir storage account

--- uksouth/storage/main.tf
+++ uksouth/storage/main.tf
@@ -106,3 +106,14 @@ resource "azurerm_container_registry" "olympus" {
         location = "ukwest"
     }
 }
+
+resource "azurerm_storage_account" "binkmimir" {
+    name = "binkmimir"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    account_kind = "StorageV2"
+    account_tier = "Standard"
+    account_replication_type = "ZRS"
+    enable_https_traffic_only = true
+    min_tls_version = "TLS1_2"
+}
