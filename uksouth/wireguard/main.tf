5363f0466ff93b2d5be33a5b493857df66da3733
Chris Pressland <mail@cpressland.io>
2022-08-02 17:32:05 +0100

    Resize wireguard

--- uksouth/wireguard/main.tf
+++ uksouth/wireguard/main.tf
@@ -162,7 +162,7 @@ resource "azurerm_linux_virtual_machine" "wireguard" {
     name = "wireguard"
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
-    size = "Standard_D2s_v4"
+    size = "Standard_D2as_v4"
     admin_username = "terraform"
     tags = var.tags
     network_interface_ids = [
