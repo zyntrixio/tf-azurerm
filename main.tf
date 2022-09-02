214b0ce5338873acca43f8fabe9406b8a32078aa
Chris Pressland <mail@cpressland.io>
2022-09-02 17:14:25 +0100

    Remove the old tools cluster

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ terraform {
     required_providers {
         azurerm = {
             source  = "hashicorp/azurerm"
-            version = "3.20.0"
+            version = "3.21.0"
         }
         chef = {
             source = "terrycain/chef"
