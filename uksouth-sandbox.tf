997caf6a084d316035d25c881ab04a2c96ef07af
Chris Pressland <mail@cpressland.io>
2022-10-27 11:27:54 +0100

    All the staging tweaks, and back again

--- uksouth-sandbox.tf
+++ uksouth-sandbox.tf
@@ -1,5 +1,5 @@
 module "uksouth_sandbox_environment" {
-    source = "github.com/binkhq/tf-azurerm_environment?ref=5.14.0"
+    source = "github.com/binkhq/tf-azurerm_environment?ref=5.15.0"
     providers = {
         azurerm = azurerm.uk_sandbox
         azurerm.core = azurerm
--- uksouth-staging.tf
+++ uksouth-staging.tf
@@ -1,5 +1,5 @@
 module "uksouth_staging_environment" {
-    source = "github.com/binkhq/tf-azurerm_environment?ref=5.14.0"
+    source = "github.com/binkhq/tf-azurerm_environment?ref=5.15.0"
     providers = {
         azurerm = azurerm.uk_staging
         azurerm.core = azurerm
