e5ebb412456ab9b8ee57365f7d112c963de9f3f3
Chris Pressland <mail@cpressland.io>
2022-11-21 13:58:13 +0000

    Updates and Tidy Up

--- uksouth/storage/main.tf
+++ uksouth/storage/main.tf
@@ -16,19 +16,6 @@ resource "azurerm_storage_account" "itops" {
     min_tls_version = "TLS1_2"
 }
 
-resource "azurerm_storage_account" "binkopsreports" {
-    name = "binkopsreports"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-
-    cross_tenant_replication_enabled = false
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
 resource "azurerm_storage_account" "bink" {
     name = "bink"
     resource_group_name = azurerm_resource_group.rg.name
@@ -74,18 +61,6 @@ resource "azurerm_storage_account" "binkarchives" {
     min_tls_version = "TLS1_2"
 }
 
-resource "azurerm_storage_account" "mids" {
-    name = "mids"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-
-    cross_tenant_replication_enabled = false
-    account_kind = "BlobStorage"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
 
 resource "azurerm_storage_account" "binkpypi" {
     name = "binkpypi"
@@ -93,47 +68,6 @@ resource "azurerm_storage_account" "binkpypi" {
     location = azurerm_resource_group.rg.location
 
     cross_tenant_replication_enabled = false
-    account_tier = "Standard"
-    account_replication_type = "ZRS"
-    enable_https_traffic_only = true
-    min_tls_version = "TLS1_2"
-}
-
-resource "azurerm_container_registry" "binkops" {
-    name = "binkops"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = "westeurope"
-    sku = "Premium"
-    admin_enabled = true
-    georeplications {
-        location = "uksouth"
-    }
-    georeplications {
-        location = "ukwest"
-    }
-}
-
-resource "azurerm_container_registry" "olympus" {
-    name = "olympus"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = "westeurope"
-    sku = "Premium"
-    admin_enabled = true
-    georeplications {
-        location = "uksouth"
-    }
-    georeplications {
-        location = "ukwest"
-    }
-}
-
-resource "azurerm_storage_account" "binkmimir" {
-    name = "binkmimir"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-
-    cross_tenant_replication_enabled = false
-    account_kind = "StorageV2"
     account_tier = "Standard"
     account_replication_type = "ZRS"
     enable_https_traffic_only = true
