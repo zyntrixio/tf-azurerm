165d14cff90c508351a3597dff27bd6c25c3f5d4
Chris Pressland <mail@cpressland.io>
2023-06-22 11:32:26 +0100

    Add Storage Account for SFTP

--- cluster/storage_common.tf
+++ cluster/storage_common.tf
@@ -26,6 +26,13 @@ resource "azurerm_key_vault_secret" "st" {
         "nfs_resource_group" = azurerm_resource_group.i.name,
         "nfs_key_primary" = azurerm_storage_account.nfs[0].primary_access_key,
         "nfs_key_secondary" = azurerm_storage_account.nfs[0].secondary_access_key,
+
+        "sftp_connection_string_primary" = azurerm_storage_account.sftp[0].primary_connection_string,
+        "sftp_connection_string_secondary" = azurerm_storage_account.sftp[0].secondary_connection_string,
+        "sftp_account_name" = azurerm_storage_account.sftp[0].name,
+        "sftp_resource_group" = azurerm_resource_group.i.name,
+        "sftp_key_primary" = azurerm_storage_account.sftp[0].primary_access_key,
+        "sftp_key_secondary" = azurerm_storage_account.sftp[0].secondary_access_key,
     })
     tags = {
         k8s_secret_name = "azure-storage"
