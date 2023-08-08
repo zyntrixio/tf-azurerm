9b68362bcf4d95032943359ced00f74e9e23e51b
Chris Pressland <mail@cpressland.io>
2023-08-08 10:30:32 +0100

    Remove help.gb.bink.com

--- uksouth/frontdoor/main.tf
+++ uksouth/frontdoor/main.tf
@@ -303,12 +303,6 @@ locals {
             "cert_name" = "acmebot-gb-bink-com"
             "origins" = {"link.prod.uksouth.bink.sh" = {"id" = local.private_link_ids.uksouth_prod}}
         }
-        "uksouth_prod_help" = {
-            "endpoint" = "uksouth-prod"
-            "domain" = "help.gb.bink.com"
-            "cert_name" = "acmebot-gb-bink-com"
-            "origins" = {"help.prod.uksouth.bink.sh" = {"id" = local.private_link_ids.uksouth_prod}}
-        }
         "uksouth_prod_bpl" = {
             "endpoint" = "uksouth-prod"
             "domain" = "bpl.gb.bink.com"
