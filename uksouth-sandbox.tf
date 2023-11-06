3c6c997e5d3c107c9824d37464aa0e1c67080d32
Thenuja Viknarajah <thenuja_1@outlook.com>
2023-11-06 14:16:32 +0000

    adding mm back to  sandbox

--- uksouth-sandbox.tf
+++ uksouth-sandbox.tf
@@ -12,6 +12,7 @@ locals {
             (local.aad_user.terraform) = { assigned_to = ["kv_su"] }
             (local.aad_group.backend) = { assigned_to = ["rg", "aks_rw", "st_rw", "kv_rw", "ac_rw"] }
             (local.aad_group.architecture) = { assigned_to = ["rg", "aks_rw", "kv_rw"] }
+            (local.aad_user.michael_morar) = { assigned_to = ["rg", "aks_rw", "kv_rw"] }
         }
         managed_identities = {
             "angelia" = { assigned_to = ["kv_ro"], namespaces = ["olympus"] }
