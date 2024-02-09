32015c5b0504a12d96d5c65d9315fc73e56e8cc8
Chris Pressland <mail@cpressland.io>
2024-02-09 19:45:54 +0000

    Allow outbound connections to Checkly Agent runners

--- uksouth/firewall/rules.tf
+++ uksouth/firewall/rules.tf
@@ -380,7 +380,7 @@ resource "azurerm_firewall_application_rule_collection" "software" {
   rule {
     name             = "Checkly"
     source_addresses = ["*"]
-    target_fqdns     = ["api.checklyhq.com", "ping.checklyhq.com"]
+    target_fqdns     = ["api.checklyhq.com", "ping.checklyhq.com", "agent.checklyhq.com"]
     protocol {
       port = "443"
       type = "Https"
