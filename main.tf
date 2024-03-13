fa06fa2635c4f4236396980929a126b50b67804e
Chris Pressland <mail@cpressland.io>
2024-03-13 00:21:54 +0000

    Remove digital ocean golink vm

--- main.tf
+++ main.tf
@@ -76,10 +76,6 @@ data "azuread_groups" "all" {
   return_all = true
 }
 
-module "golink" {
-  source = "./golink"
-}
-
 module "uksouth_core" {
   source                    = "./uksouth/core"
   loganalytics_workspace_id = module.uksouth_loganalytics.id
